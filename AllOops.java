7db8f4322cef4146dabb3be5000acf48cdd76ee0
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2025-01-10 21:38:08 +0530

    Add files via upload

---
+++ AllOops.java
@@ -0,0 +1,16 @@
+package com.oops;
+
+public class AllOops {
+
+	public static void main(String[] args) {
+		Bike b = new Bike();
+		Dio d = new Dio();
+		b.start();
+		d.start();
+		b.start(5);
+		
+	
+
+	}
+
+}
